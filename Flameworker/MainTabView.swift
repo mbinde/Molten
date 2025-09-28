e20952033563d32ba0bb179f029c2f9d7f3fa57b
mbinde <m@binde.dev>
2025-09-28 16:51:22 -0700

    Added tabs for Catalog versus Inventory

---
+++ Flameworker/MainTabView.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabView.swift
+//  Flameworker
+//
+//  Created by Melissa Binde on 9/28/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Catalog", systemImage: "books.vertical")
+                }
+            
+            InventoryView()
+                .tabItem {
+                    Label("Inventory", systemImage: "archivebox")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}

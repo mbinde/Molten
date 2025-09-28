eb69366f1a06f70046387889b15a61ff95407b79
mbinde <m@binde.dev>
2025-09-27 22:38:17 -0700

    Initial Commit

---
+++ Flameworker/FlameworkerApp.swift
@@ -0,0 +1,21 @@
+//
+//  FlameworkerApp.swift
+//  Flameworker
+//
+//  Created by Melissa Binde on 9/27/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct FlameworkerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ FlameworkerTests/FlameworkerTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlameworkerTests.swift
+//  FlameworkerTests
+//
+//  Created by Melissa Binde on 9/27/25.
+//
+
+import Testing
+
+struct FlameworkerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
